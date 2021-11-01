df4bf247ed69f55f640f10d4acb1db074cdb52a9
mrbun1906 <mrbun1906@gmail.com>
2021-11-01 09:32:21 +0700

    Hoàn thiện rule, Thêm Skill
    Xong đóng mở luật chơi, thêm png skill của tất cả nhân vật vào img. Sửa đóng mở độ khó, sửa hover

--- app.js
+++ app.js
@@ -9,6 +9,103 @@ const app = Vue.createApp({
       isShowPopup: true,
       health: 10,
       countHealth: 0,
+      listMonsters: [
+        {
+          name: "MONSTER-1",
+          srcImg: "./image/dragon-icegif.gif",
+          altImg: "monster_1"
+        },
+        {
+          name: "MONSTER-2",
+          srcImg: "./image/dragon_2.gif",
+          altImg: "monster_2"
+        },
+        {
+          name: "MONSTER-3",
+          srcImg: "./image/dragon-3.gif",
+          altImg: "monster_3"
+        },
+        {
+          name: "MONSTER-4",
+          srcImg: "./image/dragon-4.gif",
+          altImg: "monster_4"
+        },
+        {
+          name: "MONSTER-5",
+          srcImg: "./image/dragon-5.gif",
+          altImg: "monster_5"
+        },
+        {
+          name: "MONSTER-6",
+          srcImg: "./image/dragon-6.gif",
+          altImg: "monster_6"
+        },
+        {
+          name: "MONSTER-7",
+          srcImg: "./image/dragon-7.gif",
+          altImg: "monster_7"
+        },
+        {
+          name: "MONSTER-8",
+          srcImg: "./image/dragon-8.gif",
+          altImg: "monster_8"
+        }
+      ],
+      listCharacters: [
+        {
+          name: "Erhadt",
+          srcImg: "./image/User/erhardt-octopath.gif",
+          altImg: "user_1"
+        },
+        {
+          name: "ShovelKnight",
+          srcImg: "./image/User/giac_dau_si.gif",
+          altImg: "user_2"
+        },
+        {
+          name: "Arthur",
+          srcImg: "./image/User/hiep_si.gif",
+          altImg: "user_3"
+        },
+        {
+          name: "Honda",
+          srcImg: "./image/User/honda_sumo.gif",
+          altImg: "user_4"
+        },
+        {
+          name: "Rochyn",
+          srcImg: "./image/User/rochyn_idle.gif",
+          altImg: "user_5"
+        },
+        {
+          name: "Alex",
+          srcImg: "./image/User/sorcerer.gif",
+          altImg: "user_6"
+        },
+        {
+          name: "Hakan",
+          srcImg: "./image/User/Usfiv_hakan.gif",
+          altImg: "user_7"
+        },
+      ],
+      levels: [
+        {
+          key: "Dễ"
+        },
+        {
+          key: "Trung Bình"
+        },
+        {
+          key: "Khó"
+        }
+      ],
+      isShowMonster: false,
+      isShowDifficulty: false,
+      isShowRules: false,
+      selectedMonster: 0,
+      isShowCharacter: false,
+      selectedCharacter: 0,
+      selectedDifficulty: 0,
     };
   },
   methods: {
@@ -37,19 +134,58 @@ const app = Vue.createApp({
       if(this.userHeart >= 80) {
         alert("Bạn không thể hồi máu khi máu còn trên 80%");
       }
+    },
+    showMonsters() {
+      this.isShowMonster = true;
+    },
+    hideMonster() {
+      this.isShowMonster = false;
+    },
+    handleMonster(index) {
+      this.selectedMonster = index;
+      this.isShowMonster = false;
+    },
+    handleCharacter(index) {
+      this.selectedCharacter = index;
+      this.isShowCharacter = false;
+    },
+    showDifficulty() {
+      this.isShowDifficulty = true;
+    },
+    handleDifficulty(index) {
+      this.selectedDifficulty = index;
+      this.isShowDifficulty = false;
+      console.log(index);
+    },
+    showRules() {
+      this.isShowRules = true;
+    },
+    hideRule() {
+      this.isShowRules = false;
     }
   },
   computed: {
-    
+    monsterSelect() {
+      let index = this.selectedMonster;
+      return this.listMonsters[index];
+    },
+    characterSelect() {
+      let index = this.selectedCharacter;
+      return this.listCharacters[index];
+    },
+    difficulSelect() {
+      let index = this.selectedDifficulty;
+      return this.levels[index];
+    }
   },
   watch: {
     userHeart() {
-     if(this.userHeart < 20) {
+     if(this.userHeart <= 10) {
       setTimeout(function() {
-        alert("HP của bạn đang dưới 20%, cần bổ sung HP");
+        alert("HP của bạn đang dưới 10%, cần bổ sung HP");
       }, 500)
      }      
-    }
+    },
   },
 });
 
